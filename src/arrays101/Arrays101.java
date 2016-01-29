4738e37ef271934f7d7a377a645e636bbe648e95
lefty <lefty@lefty-Inspiron-5558>
2016-01-29 14:33:17 +0100

    Bubble sort and Is number in array method added

--- src/arrays101/Arrays101.java
+++ src/arrays101/Arrays101.java
@@ -31,7 +31,7 @@ public class Arrays101 {
 
     public static int[] FillArray(int array[]) {
         for (int i = 0; i < array.length; i++) {
-            System.out.println("Enter next number: ");
+            System.out.print("Enter number " + (i + 1) + ": ");
             array[i] = GetIntFromKeyboard();
 //            System.out.println(array[i]);
         }
@@ -70,28 +70,77 @@ public class Arrays101 {
         return array;
     }
 
+    public static boolean IsNumberInArray(int number, int[] array) {
+        BubbleSortArray(array);
+        int half = 0;
+        if (array.length > 1) {
+            half = (int) array.length / 2;
+        }
+
+        if (half > 0 && array[half] > number) {
+            System.out.println("less than the number in the middle");
+            IsNumberInArray(number, SubArray(half, array.length, array));
+        }
+
+        if (half > 0 && array[half] < number) {
+            System.out.println("larger than the number in the middle");
+            IsNumberInArray(number, SubArray(0, half, array));
+        }
+
+        if (array[half] == number) {
+            System.out.println("it is the number in the middle");
+            System.out.println("Returning True");
+            return true;
+        } 
+        if (array[0] == number) {
+            System.out.println("it is the number in the middle");
+            System.out.println("Returning True");
+            return true;
+        } else {
+            System.out.println("Returing FALSE");
+            return false;
+        }
+    }
+
+    public static int[] SubArray(int start_index, int last_index, int[] array) {
+        BubbleSortArray(array);
+
+        int[] new_array = new int[last_index - start_index];
+
+        for (int i = 0; i < new_array.length; i++) {
+            new_array[i] = array[start_index + i];
+        }
+
+        return new_array;
+    }
+
+    public static void PrintArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("_____________");
+        System.out.println("");
+    }
+
     public static void main(String[] args) {
+
         int[] numbers = new int[HowManyTimes("How many numbers do you want to store? ")];
 
         FillArray(numbers);
 
         System.out.println("Original array");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
-        
-        BubbleSortArray(numbers);
+        IsNumberInArray(8, numbers);
 
-        System.out.println("Sorted array");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
-
-        System.out.println("Sum: "+SumTotalOfArray(numbers));
-        System.out.println("Product: "+ProductOfArray(numbers));
-        System.out.println("Largest: "+numbers[0]);
-        System.out.println("Smallest: "+numbers[numbers.length-1]);
-        
+//        IsNumberInArray(23,numbers);
+//       BubbleSortArray(numbers);
+//
+//        System.out.println("Sorted array");
+//      PrintArray(numbers);  
+//        System.out.println("Sum: "+SumTotalOfArray(numbers));
+//        System.out.println("Product: "+ProductOfArray(numbers));
+//        System.out.println("Largest: "+numbers[0]);
+//        System.out.println("Smallest: "+numbers[numbers.length-1]);
+//        
 //        
 //        
 //        int[] array_ = new int [5];
